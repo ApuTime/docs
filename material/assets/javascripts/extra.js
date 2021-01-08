43edb95acdafe186c0b785bd44a0979545fa1f20
maschke91 <ondrej@maschke.cz>
2021-01-08 09:09:26 +0100

    homepage update

--- material/assets/javascripts/extra.js
+++ material/assets/javascripts/extra.js
@@ -1,6 +1,10 @@
 console.log("extra js init");
 
-var lightbox = new SimpleLightbox("figure a", {
+var lightbox = new SimpleLightbox("[js-lightbox-video]", {
 	captionsData: "alt",
 	captionPosition: "outside",
 });
+
+/* var lightbox = new SimpleLightbox({
+	elements: document.querySelectorAll(".lightbox a"),
+}); */
