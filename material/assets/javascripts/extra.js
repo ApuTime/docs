d592dfe242d34722d47918e45409e747e198bfc2
maschke91 <ondrej@maschke.cz>
2021-01-28 12:54:03 +0100

    reopen lighbox with form

--- material/assets/javascripts/extra.js
+++ material/assets/javascripts/extra.js
@@ -1,29 +1,52 @@
-console.log("extra js init");
-
+/**
+ * Declaration lightbox
+ */
 var lightbox = GLightbox();
 
-/** For galleries just add class .glightbox on links */
+/**
+ * Init lightbox for image galleries (a.glightbox)
+ */
 lightbox = GLightbox({
 	touchNavigation: true,
 	loop: true,
 });
 
-/** Feedback Form */
+/**
+ * Form init
+ */
+function formInit() {
+	hbspt.forms.create({
+		portalId: "7639599",
+		formId: "25e91c0c-2b5f-48cf-a104-e94e2d677510",
+		target: "#feedbackForm"
+	});
+}
+
+/**
+ * Open lightbox on click with form in content
+ */
 document
 	.getElementById("feedbackButton")
 	.addEventListener("click", function () {
 		openIframeForm(lightbox);
 	});
 
+/**
+ * Open lightbox with content
+ */
 function openIframeForm(plugin) {
 
-	var contentWrap = document.getElementById("feedbackForm");
+	var contentWrap = document.getElementsByClassName("hs-form-iframe")[0];
 
-	plugin.setElements([
-		{
-			content: contentWrap,
-		},
-	]);
-
+	plugin.setElements([{ content: contentWrap }]);
 	plugin.open();
+
+	formInit();
 }
+
+/**
+ * Init section after DOMContentLoaded
+ */
+document.addEventListener("DOMContentLoaded", function (event) {
+	formInit();
+});
