c82ce363e30d19000c766d3f56dd874d2e79dea3
Martin Lonský <martin.lonsky@myappin.com>
2022-08-09 10:53:46 +0200

    remove feedback

--- material/assets/javascripts/extra.js
+++ material/assets/javascripts/extra.js
@@ -34,49 +34,8 @@ lightbox = GLightbox({
 });
 
 /**
- * Form init
- */
-function formInit() {
-	hbspt.forms.create({
-		portalId: "7639599",
-		formId: "25e91c0c-2b5f-48cf-a104-e94e2d677510",
-		target: "#feedbackForm"
-	});
-}
-
-/**
  * Open vimeo iframe
  */
 var lightboxDescription = GLightbox({
 	selector: ".video-lightbox",
-});
-
-/**
- * Open lightbox on click with form in content
- */
-if (document.getElementById("feedbackButton")) {
-	document
-		.getElementById("feedbackButton")
-		.addEventListener("click", function () {
-			openIframeForm(lightbox);
-		});
-}
-/**
- * Open lightbox with content
- */
-function openIframeForm(plugin) {
-
-	var contentWrap = document.getElementsByClassName("hs-form-iframe")[0];
-
-	plugin.setElements([{ content: contentWrap }]);
-	plugin.open();
-
-	formInit();
-}
-
-/**
- * Init section after DOMContentLoaded
- */
-document.addEventListener("DOMContentLoaded", function (event) {
-	formInit();
 });
